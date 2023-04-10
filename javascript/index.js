e0396d98765a34db8d0cf66f8bb4dd32dc86913c
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2023-04-10 11:18:39 +0100

    fetch front end  when open index

--- javascript/index.js
+++ javascript/index.js
@@ -4,3 +4,11 @@ fetch("https://smartluggagebackend.herokuapp.com/api/v1/trip").then((response) =
     //setTripData(TripData);
     // calculateLastTripId();
 });
+
+
+fetch("https://smartluggage.herokuapp.com/").then((response) => response.json()).then((TripData) => {
+    // fetch("api/v1/trip/").then((response) => response.json()).then((TripData) => {
+    // console.log("List of items in the trip", TripData);
+    //setTripData(TripData);
+    // calculateLastTripId();
+});
