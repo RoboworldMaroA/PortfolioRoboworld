d11b6e3fa52abac9738931ac07c5019fe5e6c258
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2024-10-13 18:03:52 +0100

    add carousel to automations and robotics

---
+++ javascript/automations.js
@@ -0,0 +1,33 @@
+// var myCarousel = document.querySelector('#myCarousel')
+// var carousel = new bootstrap.Carousel(myCarousel)
+
+
+
+function typeWriter(text, elementId, delay) {
+    let i = 0;
+    const element = document.getElementById(elementId);
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, delay);
+        }
+    }
+    type();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const element = entry.target;
+                element.classList.remove('hidden');
+                typeWriter("ABB Robots", "typewriter", 300);
+                observer.unobserve(element);
+            }
+        });
+    });
+
+    const target = document.getElementById('typewriter');
+    observer.observe(target);
+});
